3106d680224a55dddbb551e65eb37ffe028dc300
kevingunawanc14 <c14200005@john.petra.ac.id>
2024-04-29 17:18:35 +0700

    push-up

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 2000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
